0a308b395efc0885df5b19bfd2aab7fafb65aabf
Obinna <obinna@fivenines.com.ng>
2020-09-20 23:00:25 +0100

    add read file method

--- src/Service/FileConverter.php
+++ src/Service/FileConverter.php
@@ -6,9 +6,32 @@ use Symfony\Component\HttpFoundation\File\File;
 
 class FileConverter
 {
-    private $file;
+    /**
+     * Decode file contents and return as array
+     * @param File $file 
+     * @return array
+     * @throws Exception if file format not supported
+     */
+    public static function read($file)
+    {
+        $fh = $file->openFile('r');
+        $content = $fh->fread($fh->getSize());
+        
+        switch ($file->getExtension()) {
+            case 'json':
+                $data = json_decode($content, true);
+                break;
+            case 'xml':
+                $xml = simplexml_load_string($content);
+                $json = json_encode($xml);
+                $data = json_decode($json,TRUE)['hotel'];
+                break;
+            // Add more file types
+            default:
+                throw new \Exception("File format not supported");
+                break;
+        }
 
-    function __construct($filename) {
-        $this->file = new File($filename, false);
+        return $data;
     }
 }
