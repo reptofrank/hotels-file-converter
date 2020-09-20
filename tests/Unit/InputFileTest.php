4615b6221c0fb0277df46c7acea19ff16d5657c3
Obinna <obinna@fivenines.com.ng>
2020-09-20 22:54:47 +0100

    file read tests

--- tests/Unit/InputFileTest.php
+++ tests/Unit/InputFileTest.php
@@ -3,11 +3,27 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Service\FileConverter;
+use Symfony\Component\HttpFoundation\File\File;
 
 class InputFileTest extends TestCase
 {
-    public function inputFileExists()
+    
+    public function testFileReadWithJSON()
     {
-        $this->assertTrue(true);
+        $filePath = __DIR__ . '/hotels.json';
+        $file = new File($filePath);
+        $data = FileConverter::read($file);
+        $this->assertIsArray($data);
+        $this->assertCount(13, $data);
+    }
+
+    public function testFileReadWithXML()
+    {
+        $filePath = __DIR__ . '/hotels.xml';
+        $file = new File($filePath);
+        $data = FileConverter::read($file);
+        $this->assertIsArray($data);
+        $this->assertCount(15, $data);
     }
 }
